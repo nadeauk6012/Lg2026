2af7d5ce7ac5b2903624bcd00f151bd631a76571
nadeauk6012 <26035422+nadeauk6012@users.noreply.github.com>
2026-02-12 00:33:47 -0500

    Update revision_data.h.in.cmake

--- revision_data.h.in.cmake
+++ revision_data.h.in.cmake
@@ -4,16 +4,16 @@
  #define _DATE                      "@rev_date@"
  #define _BRANCH                    "@rev_branch@"
  #define _CMAKE_COMMAND             R"(@CMAKE_COMMAND@)"
- #define _SOURCE_DIRECTORY          R"(@CMAKE_SOURCE_DIR@)"
+ #define _CMAKE_VERSION             R"(@CMAKE_VERSION@)"
  #define _CMAKE_HOST_SYSTEM         R"(@CMAKE_HOST_SYSTEM_NAME@ @CMAKE_HOST_SYSTEM_VERSION@)"
+ #define _SOURCE_DIRECTORY          R"(@CMAKE_SOURCE_DIR@)"
  #define _BUILD_DIRECTORY           R"(@BUILDDIR@)"
  #define _MYSQL_EXECUTABLE          R"(@MYSQL_EXECUTABLE@)"
- #define _FULL_DATABASE             "BH_world_735_26124_2024_09_24.sql"
- #define _HOTFIXES_DATABASE         "BH_hotfixes_26124_2024_09_24.sql"
- #define VER_COMPANYNAME_STR        "TrinityCore Developers"
- #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2026 TrinityCore"
- #define VER_FILEVERSION            0,4,0
- #define VER_FILEVERSION_STR        "@rev_hash@ @rev_date@ (@rev_branch@ branch)"
+ #define _FULL_DATABASE             "DB_World_735.02.sql"
+ #define _HOTFIXES_DATABASE         "DB_Hotfixes_735.02.sql"
+ #define VER_COMPANYNAME_STR        "BHCORE"
+ #define VER_LEGALCOPYRIGHT_STR     "(c)2026 BHCORE"
+ #define VER_FILEVERSION            0,4,2
  #define VER_FILEVERSION_STR        "BHCORE"
  #define VER_PRODUCTVERSION         VER_FILEVERSION
  #define VER_PRODUCTVERSION_STR     VER_FILEVERSION_STR
