19bef4e0dc7aad3f6dd20f85a12fd26595bdc859
nadeauk6012 <26035422+nadeauk6012@users.noreply.github.com>
2026-02-13 23:53:14 -0500

    Loading quick

--- revision_data.h.in.cmake
+++ revision_data.h.in.cmake
@@ -9,8 +9,8 @@
  #define _SOURCE_DIRECTORY          R"(@CMAKE_SOURCE_DIR@)"
  #define _BUILD_DIRECTORY           R"(@BUILDDIR@)"
  #define _MYSQL_EXECUTABLE          R"(@MYSQL_EXECUTABLE@)"
- #define _FULL_DATABASE             "DB_World_735.02.sql"
- #define _HOTFIXES_DATABASE         "DB_Hotfixes_735.02.sql"
+ #define _FULL_DATABASE             "BHDB v.2.1_World.sql"
+ #define _HOTFIXES_DATABASE         "BHDB v.2.1_Hotfixes.sql"
  #define VER_COMPANYNAME_STR        "BHCORE"
  #define VER_LEGALCOPYRIGHT_STR     "(c)2026 BHCORE"
  #define VER_FILEVERSION            0,4,2
