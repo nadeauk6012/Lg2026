459076ae9cad616c3b9140ab4e9fa34732b6ba45
nadeauk6012 <26035422+nadeauk6012@users.noreply.github.com>
2026-02-25 00:14:28 -0500

    s

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -19564,7 +19564,6 @@ bool Unit::SpellProcTriggered(Unit* victim, DamageInfo* dmgInfoProc, AuraEffect*
                 target = summon;
             }
 
-
             Unit* _caster = this;
             Unit* _targetAura = this;
             Unit* _targetAura2 = this;
