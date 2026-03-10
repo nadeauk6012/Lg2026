f154394e8201c4f5cf5a5c3ab5dfbd8d018576f1
nadeauk6012 <26035422+nadeauk6012@users.noreply.github.com>
2026-03-10 16:32:51 -0400

    Update World.cpp

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -4247,9 +4247,9 @@ void World::LoadCharacterNameData()
     do
     {
         Field* fields = result->Fetch();
-        AddCharacterInfo(ObjectGuid::Create<HighGuid::Player>(fields[0].GetUInt64()), fields[6].GetUInt32() /*account*/, fields[1].GetString()
+        AddCharacterInfo(ObjectGuid::Create<HighGuid::Player>(fields[0].GetUInt64()), fields[6].GetUInt32() /*account*/, fields[1].GetString(),
             fields[3].GetUInt8() /*gender*/, fields[2].GetUInt8() /*race*/, fields[4].GetUInt8() /*class*/, fields[5].GetUInt8() /*level*/,
-            fields[7].GetUInt16() /*zoneId*/, fields[8].GetUInt32() /*rankId*/, ObjectGuid::Create<HighGuid::Guild>(fields[9].GetUInt32()) /*guildId*/, (fields[10].GetUInt32()) /*SpecId*/);
+            fields[7].GetUInt16() /*zoneId*/, fields[8].GetUInt32() /*rankId*/, ObjectGuid::Create<HighGuid::Guild>(fields[9].GetUInt32()) /*guildId*/, fields[10].GetUInt16() /*SpecId*/);
         ++count;
     } while (result->NextRow());
 
